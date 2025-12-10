331e1d0b976cfa5e3eb31d1cdbd0b9627e076b5c
류현 <2022011870@cju.ac.kr>
2025-12-10 17:25:48 +0900

    feat:1463 bfs로도 가능할 것 같음

---
+++ untitled/src/main/java/org/example/dp/_1463.java
@@ -0,0 +1,21 @@
+package org.example.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class _1463 {//dp라고는 하지만 bfs도 될 듯
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer N = Integer.parseInt(br.readLine());
+        //N%3==0 > n/3
+        //N%2==0 > n/2
+        //N-1
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+
+
+    }
+}
