7f2bb0b912d21dc6c44c3faeb1070f71bbe2ab57
류현 <hyen4320@naver.com>
2025-12-14 00:57:09 +0900

    feat: bronze bruteforce 11050 and 2108 trying

--- untitled/src/main/java/org/example/implementation/_2108.java
+++ untitled/src/main/java/org/example/implementation/_2108.java
@@ -1,8 +1,7 @@
 package org.example.implementation;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class _2108 {
     public static void main(String[] args) throws IOException {
@@ -10,16 +9,24 @@ public class _2108 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String s = br.readLine();
         ArrayList<Integer> list = new ArrayList<>();
-        int bigNum=0;
-        int countNum=0;
         for(int i=0;i<Integer.parseInt(s);i++){
             list.add(Integer.parseInt(br.readLine()));
         }
-
-        list.sort(Integer::compareTo);
+        ArrayList<Integer> countList = new ArrayList<>();
+        for(int i=0; i<list.size();i++){
+            int count=0;
+            for(int j=0;j<list.size();j++){
+                if(list.get(i)==list.get(j)){//값만 비교
+                    count++;
+                }
+            }
+            countList.add(count);
+        }
+        Collections.sort(countList);
+        Collections.sort(list);
         System.out.println("산술평균 " + (list.stream().mapToInt(Integer::intValue).sum()/list.size()));
         System.out.println("중앙값" + list.toArray()[list.size()/2]);
-        System.out.println("최빈값");
-        System.out.println("범위");
+        System.out.println("최빈값 "+ countList.get(list.size()-1));
+        System.out.println("범위 " + (list.get(list.size()-1)-list.get(0)));
     }
 }
