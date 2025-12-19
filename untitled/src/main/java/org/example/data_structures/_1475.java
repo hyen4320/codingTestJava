b444362a76c44ebb79c7186c2956dcf762bd7dcf
류현 <hyen4320@naver.com>
2025-12-20 02:31:51 +0900

    feat: 1475 trying

---
+++ untitled/src/main/java/org/example/data_structures/_1475.java
@@ -0,0 +1,22 @@
+package org.example.data_structures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class _1475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String roomNumber = br.readLine();
+        roomNumber = roomNumber.replaceAll("6","9");
+        Double price = 0.0;
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(char i:roomNumber.toCharArray()){
+        }
+        System.out.println("price = " + price);
+
+
+    }
+}
