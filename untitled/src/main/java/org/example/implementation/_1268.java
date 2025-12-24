263029f2edb96d44651a83c44ca38e0e21ec3d2a
류현 <2022011870@cju.ac.kr>
2025-12-24 11:06:33 +0900

    feat:접근방식이 틀려서 갈아엎음 1학년 때 3반이고 3학년 때도 3반일 수 있음

--- untitled/src/main/java/org/example/implementation/_1268.java
+++ untitled/src/main/java/org/example/implementation/_1268.java
@@ -3,13 +3,25 @@ package org.example.implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class _1268 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList();
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0;j<5;j++){
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        int index = 0;
+        int loop=0;
+        while(loop<N*5){
+            list.stream();
+            index++;
+        }
 
     }
 }
