f4a6191be4795119bdf983225d296fff2c5b0a55
류현 <2022011870@cju.ac.kr>
2025-12-26 15:34:54 +0900

    feat:_1475 clear

--- untitled/src/main/java/org/example/implementation/_1475.java
+++ untitled/src/main/java/org/example/implementation/_1475.java
@@ -4,18 +4,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class _1475 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String roomNumber = br.readLine();
-        roomNumber = roomNumber.replaceAll("6","9");
-        Double price = 0.0;
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for(char i:roomNumber.toCharArray()){
+        ArrayList<Integer> plasticNumbers = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+        int count = 1;
+
+        for (Character i : roomNumber.toCharArray()) {
+            if (plasticNumbers.contains(Integer.parseInt(i.toString()))) {
+                plasticNumbers.remove(plasticNumbers.indexOf(Integer.parseInt(i.toString())));
+            }
+            else {
+                if (i.toString().equals("6")) {
+                    if(plasticNumbers.contains(9)){
+                        plasticNumbers.remove(plasticNumbers.indexOf(Integer.parseInt("9")));
+                    }
+                    else{
+                        count++;
+                        plasticNumbers.addAll(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+                        plasticNumbers.remove(plasticNumbers.indexOf(Integer.parseInt(i.toString())));
+                    }
+                }
+                else if (i.toString().equals("9")) {
+                    if(plasticNumbers.contains(6)){
+                        plasticNumbers.remove(plasticNumbers.indexOf(Integer.parseInt("6")));
+                    }
+                    else{
+                        count++;
+                        plasticNumbers.addAll(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+                        plasticNumbers.remove(plasticNumbers.indexOf(Integer.parseInt(i.toString())));
+                    }
+                }
+                else{
+                    count++;
+                    plasticNumbers.addAll(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+                    plasticNumbers.remove(plasticNumbers.indexOf(Integer.parseInt(i.toString())));
+                }
+            }
         }
-        System.out.println("price = " + price);
-
-
+        System.out.println(count);
     }
 }
