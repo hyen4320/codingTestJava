feafc0172fe43cbb23e77470393458f5f565eaae
류현 <2022011870@cju.ac.kr>
2025-12-26 14:34:31 +0900

    feat:style _1475 data_structure->implementation

--- untitled/src/main/java/org/example/implementation/_1475.java
+++ untitled/src/main/java/org/example/implementation/_1475.java
@@ -1,10 +1,9 @@
-package org.example.data_structures;
+package org.example.implementation;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class _1475 {
     public static void main(String[] args) throws IOException {
